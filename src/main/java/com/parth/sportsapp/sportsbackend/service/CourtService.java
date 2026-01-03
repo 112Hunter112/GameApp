0ad2161ca559e2f927514272070ac09ceac77b7f
Parth <parth.aditya01@gmail.com>
2026-01-03 21:21:10 +0200

    Court Service created

---
+++ src/main/java/com/parth/sportsapp/sportsbackend/service/CourtService.java
@@ -0,0 +1,14 @@
+package com.parth.sportsapp.sportsbackend.service;
+
+import com.parth.sportsapp.sportsbackend.repository.CourtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourtService {
+
+  @Autowired
+  private CourtRepository courtRepository;
+
+
+}
