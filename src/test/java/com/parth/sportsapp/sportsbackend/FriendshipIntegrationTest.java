fa6dc2566820cb04c8cae932640ea844d483b85a
Parth <parth.aditya01@gmail.com>
2026-01-13 00:10:33 +0200

    Changes to pom.xml

---
+++ src/test/java/com/parth/sportsapp/sportsbackend/FriendshipIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.parth.sportsapp.sportsbackend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class FriendshipIntegrationTest {
+}
